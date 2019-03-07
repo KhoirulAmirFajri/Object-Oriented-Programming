3a3bcaf6023e456a794fcdb680dbfdcef55e7419
Khoirul Amir Fajri <48266211+KhoirulAmirFajri@users.noreply.github.com>
2019-03-07 19:27:38 +0700

    object oriented programming
    tugas pertama

---
+++ Khoirul_Amir_Fajri_17090033_4A.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_OOP;
+
+/**
+ *
+ * @author fajriompong
+ */
+public class Khoirul_Amir_Fajri_17090033_4A {
+    public static void main(String[] args){
+        System.out.println("Selamat datang di Object Oriented Programming \n Khoirul_Amir_Fajri_17090033_4A");
+    }
+    
+}
